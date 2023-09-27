a70dcb4ff7c37286910f921345b9aa1b332c1e85
darrpama <darrpama@mail.com>
2023-09-27 18:30:24 +0300

    add parser first template

--- src/models/parser.cpp
+++ src/models/parser.cpp
@@ -1,8 +1,40 @@
-#include <iostream>
-#include <string>
+#include "parser.h"
 
+using namespace s21;
 
-static Model LoadModel(const std::string filename)
+void Parser::parse()
 {
+  std::ifstream file(filename);
+  std::string line;
 
+  while (std::getline(file, line))
+  {
+    if (line.substr(0, 2) == "v ")
+    {
+      Vertex v;
+      std::sscanf(line.c_str(), "v %f %f %f", &v.x, &v.y, &v.z);
+      vertices.push_back(v);
+    }
+    else if (line.substr(0, 3) == "vn ")
+    {
+      Normal n;
+      std::sscanf(line.c_str(), "vn %f %f %f", &n.x, &n.y, &n.z);
+      normals.push_back(n);
+    }
+    else if (line.substr(0, 3) == "vt ")
+    {
+      TextureCoordinate t;
+      std::sscanf(line.c_str(), "vt %f %f", &t.u, &t.v);
+      textureCoordinates.push_back(t);
+    }
+    else if (line.substr(0, 2) == "f ")
+    {
+      Face f;
+      std::sscanf(line.c_str(), "f %d/%d/%d %d/%d/%d %d/%d/%d",
+        &f.v1, &f.t1, &f.n1,
+        &f.v2, &f.t2, &f.n2,
+        &f.v3, &f.t3, &f.n3);
+      faces.push_back(f);
+    }
+  }
 }
--- src/models/parser.h
+++ src/models/parser.h
@@ -11,27 +11,41 @@ namespace s21
 
 struct Vertex
 {
-    float x, y, z;
+  float x, y, z;
 };
 
 struct Normal
 {
-    float x, y, z;
+  float x, y, z;
 };
 
 struct TextureCoordinate
 {
-    float u,v;
+  float u,v;
 };
 
 struct Face
 {
-    int v1, v2, v3;
-    int n1, n2, n3;
-    int t1, t2, t3;
+  int v1, v2, v3;
+  int n1, n2, n3;
+  int t1, t2, t3;
 };
 
 
+
+class Parser
+{
+  public:
+    Parser(const std::string& filename) : filename(filename) {};
+    void parse();
+
+  private:
+    std::string filename;
+    std::vector<Vertex> vertices;
+    std::vector<Normal> normals;
+    std::vector<TextureCoordinate> textureCoordinates;
+    std::vector<Face> faces;
+};
 
 }
 
